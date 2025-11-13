70af9495588654041be973c58bdeec33e344b5e3
Polina <noskovichpolina@gmail.com>
2025-11-13 15:30:32 +0300

    refactor(modules): Create BooksModule and CoreModule

--- src/app/books/books-module.ts
+++ src/app/books/books-module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { BooksRoutingModule } from './books-routing-module';
-import { Books } from './components/books/books';
-import { BookList } from './components/book-list/book-list';
-import { Highlight } from './directives/highlight';
-import { HighlightPipe } from './pipes/highlight-pipe';
+import { BooksPageModule } from './books-page/books-page-module';
 
 @NgModule({
   declarations: [
-    Books,
-    BookList,
-    Highlight,
-    HighlightPipe,
   ],
   imports: [
     CommonModule,
-    BooksRoutingModule
-  ],
-  exports: [ BookList ],
+    BooksRoutingModule,
+    BooksPageModule
+  ]
 })
 export class BooksModule { }
