2c8b2b541c3e28153d64ebb3e0280503ef173258
Polina <noskovichpolina@gmail.com>
2025-11-14 12:00:08 +0300

    refactor(service): Make BookService reactive with RxJS

--- src/app/books/books-page/books-page.ts
+++ src/app/books/books-page/books-page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BookService } from '../services/book'; 
 import { Book } from '../models/book-model'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-books',
@@ -8,21 +9,31 @@ import { Book } from '../models/book-model'
   templateUrl: './books-page.html',
   styleUrl: './books-page.scss',
 })
-export class BooksPage implements OnInit {
+export class BooksPage implements OnInit, OnDestroy {
   private allBooks: Book[] = [];
   public filteredBooks: Book[] = [];
   public searchTerm: string = '';
+  private bookSubscription: Subscription = new Subscription();
 
   constructor(private bookService: BookService) {}
 
   public ngOnInit(): void {
-    this.allBooks = this.bookService.getBooks();
-    this.filteredBooks = [...this.allBooks];
+    this.bookSubscription = this.bookService.getBooks().subscribe(books => {
+      this.allBooks = books;
+      this.search(this.searchTerm);
+    })
   }
 
-  public search(value: string): void {
+  search(value: string): void {
     this.searchTerm = value.toLowerCase();
-    this.filterBooks(); 
+    if (!this.searchTerm) {
+      this.filteredBooks = [...this.allBooks];
+    } else {
+      this.filteredBooks = this.allBooks.filter(book =>
+        book.name.toLowerCase().includes(this.searchTerm) ||
+        book.type.toLowerCase().includes(this.searchTerm)
+      );
+    }
   }
 
   public createNewBook(): void {
@@ -37,20 +48,14 @@ export class BooksPage implements OnInit {
       createdAt: new Date(),
       pages: 10,
     };
-    this.allBooks.push(newBook);
-    this.filterBooks();
+    this.bookService.addBook(newBook);
   }
 
   public handleDelete(bookId: number): void {
-    this.allBooks = this.allBooks.filter(book => book.id !== bookId);
-    this.filterBooks();
+    this.bookService.deleteBook(bookId);
   }
 
-  private filterBooks(): void {
-    this.filteredBooks = this.allBooks.filter(book =>
-      !this.searchTerm ||
-      (book.name.toLowerCase().includes(this.searchTerm) ||
-       book.type.toLowerCase().includes(this.searchTerm))
-    );
+  ngOnDestroy(): void {
+    this.bookSubscription.unsubscribe();
   }
 }
