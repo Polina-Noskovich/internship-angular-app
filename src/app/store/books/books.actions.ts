7791f7d02cc244fff5127355181efd36fe5f38bc
Polina <noskovichpolina@gmail.com>
2025-11-25 09:51:10 +0300

    style(state): Adjust action type naming to singular form

--- src/app/store/books/books.actions.ts
+++ src/app/store/books/books.actions.ts
@@ -1,16 +1,16 @@
 import { Book } from "./books-state.model";
 
 export class GetBooks {
-    public static readonly type = '[Books] Get books';
+    public static readonly type = '[Books] Get book';
 }
 
 export class AddBook {
-    public static readonly type = '[Books] Add books';
+    public static readonly type = '[Books] Add book';
     constructor(public payload: Book) {}
 }
 
 export class DeleteBook {
-    public static readonly type = '[Books] Delete books';
+    public static readonly type = '[Books] Delete book';
     constructor(public payload: number) {}
 }
 
