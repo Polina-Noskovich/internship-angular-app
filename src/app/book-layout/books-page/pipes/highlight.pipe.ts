6140d694d70160f5e6ab6267fda5c8cfa79f6b31
Polina <noskovichpolina@gmail.com>
2025-11-26 12:07:56 +0300

    style(pipes): Align parameter naming in HighlightPipe

--- src/app/book-layout/books-page/pipes/highlight.pipe.ts
+++ src/app/book-layout/books-page/pipes/highlight.pipe.ts
@@ -9,11 +9,11 @@ export class HighlightPipe implements PipeTransform {
 
   public constructor(private readonly sanitizer: DomSanitizer) {}
 
-  public transform(text: string, searchTerm: string, color: string = '#cceeff'): SafeHtml {
-    if (!searchTerm || !text) {
+  public transform(text: string, searchValue: string, color: string = '#cceeff'): SafeHtml {
+    if (!searchValue || !text) {
       return text;
     }
-    const regex = new RegExp(searchTerm, 'gi');
+    const regex = new RegExp(searchValue, 'gi');
     const highlightedText = text.replace(regex, (match) => `<span style="background-color: ${color};">${match}</span>`);
 
     return this.sanitizer.bypassSecurityTrustHtml(highlightedText);
